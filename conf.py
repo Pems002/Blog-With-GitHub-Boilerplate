59b63478eab0a710c64525a423ec4c33028c2c13
神奇的Sean <1920270172@qq.com>
2020-07-25 13:48:11 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Sean's Blog"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_build_date = "2019-07-30T09:00+08:00"
+author = "Sean"
+email = "admin@imsean.cn"
+author_homepage = "https://www.imsean.cn"
+description = "乘风破浪"
+key_words = ['Blog', 'Sean', 'Maverick']
 language = 'zh-CN'
 external_links = [
     {
