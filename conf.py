ae3a90dc058073b935b599bed7334f60947adfe3
神奇的Sean <1920270172@qq.com>
2020-07-26 00:01:22 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,11 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
+template = {
     "name": "Galileo",
-    "type": "local",
-    "path": "../Galileo"
+    "type": "git",
+    "url": "https://github.com/Pems002/Maverick-Theme-Galileo.git",
+    "branch": "latest"
 }
 enable_jsdelivr = {
     "enabled": True,
